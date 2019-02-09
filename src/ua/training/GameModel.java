6e9753bb0581b5a7371f701aae8d496095c76717
derfulim <barashenko@ukr.net>
2019-02-09 16:58:41 +0200

    Записал метод получения случайного числа позволяющий получить все значения диапазона.

--- src/ua/training/GameModel.java
+++ src/ua/training/GameModel.java
@@ -1,9 +1,15 @@
 package ua.training;
 
 public class GameModel {
-
-    protected long getNumber(int minRangeLimit, int maxRangeLimit) {
-
-        return Math.round(Math.random()*(maxRangeLimit-minRangeLimit));
+    /**
+     * Метод возвращает случайное число в диапазоне ограниченом аргументами.
+     * Такая запись нужна чтобы обеспечить появление всех чисел в диапазоне включая границы.
+     * @param minRangeLimit
+     * @param maxRangeLimit
+     * @return
+     */
+    protected int getNumber(int minRangeLimit, int maxRangeLimit) {
+        int range = maxRangeLimit - minRangeLimit;
+        return (int)(Math.random() * ++range) + minRangeLimit;
     }
 }
