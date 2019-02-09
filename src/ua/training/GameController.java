86d001f6bfc7592e9a7f6c757cf5a4af1e7a4d60
derfulim <barashenko@ukr.net>
2019-02-09 17:25:25 +0200

    Записал метод получения случайного числа позволяющий получить все значения диапазона.

--- src/ua/training/GameController.java
+++ src/ua/training/GameController.java
@@ -4,6 +4,7 @@ public class GameController {
     //порядок следования переменных ссылочных и примитивов?
     private int minRangeLimit = 0;
     private int maxRangeLimit = 100;
+    private boolean isGameOver = false;
 
     private GameModel gameModel;
     private GameView gameView;
--- src/ua/training/Test.java
+++ src/ua/training/Test.java
@@ -1,4 +1,22 @@
 package ua.training;
 
 public class Test {
+     int iterations = 100;
+
+    private GameModel gameModel;
+    private GameController gameController;
+    private GameView gameView;
+
+    public Test() {
+        this.gameModel = new GameModel();
+    }
+
+    void testRandom() {
+        int array[] = new int[iterations];
+
+        for (int i = iterations-1; i>=0; i--) {
+            array[i] = gameModel.getNumber(0, 100);
+        }
+
+    }
 }
