76b4d73451242b1bcc065e9f5118125390c1315a
derfulim <barashenko@ukr.net>
2019-02-09 15:49:07 +0200

    Добавил переменные диапазона, первую версию метода получения случайного числав

--- src/ua/training/GameController.java
+++ src/ua/training/GameController.java
@@ -1,4 +1,11 @@
 package ua.training;
 
 public class GameController {
+    //порядок следования переменных ссылочных и примитивов?
+    private int minRangeLimit = 0;
+    private int maxRangeLimit = 100;
+
+    private GameModel gameModel;
+    private GameView gameView;
+
 }
--- src/ua/training/GameModel.java
+++ src/ua/training/GameModel.java
@@ -1,4 +1,9 @@
 package ua.training;
 
 public class GameModel {
+
+    protected long getNumber(int minRangeLimit, int maxRangeLimit) {
+
+        return Math.round(Math.random()*(maxRangeLimit-minRangeLimit));
+    }
 }
